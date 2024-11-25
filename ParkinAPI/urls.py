d1a5ad14b5092b4773449b66fb47a66d500f8bab
camilo177 <juanc.lopezca@campusucc.edu.co>
2024-11-24 20:31:25 -0500

    Initial commit

---
+++ ParkinAPI/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin  
+from django.urls import path, include
+from rest_framework import routers
+from AppParking.views import *
+
+router = routers.DefaultRouter()
+router.register('parqueadero', ParqueaderoView)
+router.register('usuario', UsuarioView)
+router.register('tarifa', TarifaView)
+router.register('propietario', PropietarioView, basename='propietario')
+router.register('vehiculo', VehiculoView)
+router.register('entradasalida', EntradaSalidaView)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),  
+    path('api-auth/', AuthTokenView.as_view()),
+    path('propietariobyedad', PropietarioByEdad.as_view()),
+]
